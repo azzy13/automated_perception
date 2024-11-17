fa873930fe9eefb7665bb948bd0614cf79329937
Azhar Hasan <azhar.hasan@vanderbilt.edu>
2024-11-17 03:08:20 -0600

    Losses more or less ready.

---
+++ losses/yolox_iou/__init__.py
@@ -0,0 +1,115 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from .. import yolox_utils as yolox_utils
+
+class IOUloss(nn.Module):
+    def __init__(self, reduction="none", loss_type="iou"):
+        super(IOUloss, self).__init__()
+        self.reduction = reduction
+        self.loss_type = loss_type
+
+    def forward(self, pred, target):
+        assert pred.shape[0] == target.shape[0]
+
+        pred = pred.view(-1, 4)
+        target = target.view(-1, 4)
+        tl = torch.max(
+            (pred[:, :2] - pred[:, 2:] / 2), (target[:, :2] - target[:, 2:] / 2)
+        )
+        br = torch.min(
+            (pred[:, :2] + pred[:, 2:] / 2), (target[:, :2] + target[:, 2:] / 2)
+        )
+
+        area_p = torch.prod(pred[:, 2:], 1)
+        area_g = torch.prod(target[:, 2:], 1)
+
+        en = (tl < br).type(tl.type()).prod(dim=1)
+        area_i = torch.prod(br - tl, 1) * en
+        iou = (area_i) / (area_p + area_g - area_i + 1e-16)
+
+        if self.loss_type == "iou":
+            loss = 1 - iou ** 2
+        elif self.loss_type == "giou":
+            c_tl = torch.min(
+                (pred[:, :2] - pred[:, 2:] / 2), (target[:, :2] - target[:, 2:] / 2)
+            )
+            c_br = torch.max(
+                (pred[:, :2] + pred[:, 2:] / 2), (target[:, :2] + target[:, 2:] / 2)
+            )
+            area_c = torch.prod(c_br - c_tl, 1)
+            giou = iou - (area_c - area_i) / area_c.clamp(1e-16)
+            loss = 1 - giou.clamp(min=-1.0, max=1.0)
+
+        if self.reduction == "mean":
+            loss = loss.mean()
+        elif self.reduction == "sum":
+            loss = loss.sum()
+
+        return loss
+
+
+class Loss(nn.Module):
+    def __init__(self, loss_cfg):
+        super().__init__()
+        self.loss_cfg = loss_cfg
+        self.iou_loss = IOUloss(reduction="none")
+
+    def forward(self, y_hat, y):
+        return self.get_losses(**y_hat, y)
+    
+    def get_losses(self, imgs,
+            x_shifts,
+            y_shifts,
+            expanded_strides,
+            labels,
+            outputs,
+            origin_preds,
+            dtype, y):
+        bbox_preds = outputs[:, :, :4]  # [batch, n_anchors_all, 4]
+        obj_preds = outputs[:, :, 4].unsqueeze(-1)  # [batch, n_anchors_all, 1]
+        cls_preds = outputs[:, :, 5:]  # [batch, n_anchors_all, n_cls]
+
+        origin_preds = torch.cat(origin_preds, 1)
+        fg_masks = []
+        reg_targets = []
+        num_fg = 0.0
+        num_gts = 0.0
+    
+        # calculate targets
+        mixup = labels.shape[2] > 5
+        if mixup:
+            label_cut = labels[..., :5]
+        else:
+            label_cut = labels
+        nlabel = (label_cut.sum(dim=2) > 0).sum(dim=1)  # number of objects
+        total_num_anchors = outputs.shape[1]
+
+        for batch_idx in range(outputs.shape[0]):
+            num_gt = int(nlabel[batch_idx])
+            num_gts += num_gt
+            if num_gt == 0:
+                reg_target = outputs.new_zeros((0, 4))
+            else:
+                gt_bboxes_per_image = labels[batch_idx, :num_gt, 1:5]
+                gt_classes = labels[batch_idx, :num_gt, 0]
+                bboxes_preds_per_image = bbox_preds[batch_idx]
+                (gt_matched_classes, fg_mask, pred_ious_this_matching, matched_gt_inds, num_fg_img) = yolox_utils.get_assignments(batch_idx, num_gt, total_num_anchors, gt_bboxes_per_image, gt_classes,
+                bboxes_preds_per_image, expanded_strides, x_shifts, y_shifts,
+                cls_preds, bbox_preds, obj_preds, labels, imgs)
+                reg_target = gt_bboxes_per_image[matched_gt_inds]
+
+            reg_targets.append(reg_target)
+            fg_masks.append(fg_mask)
+            
+
+        fg_masks = torch.cat(fg_masks, 0)
+        reg_targets = torch.cat(reg_targets, 0)
+        num_fg = max(num_fg, 1)
+
+        loss_iou = (
+            self.iou_loss(bbox_preds.view(-1, 4)[fg_masks], reg_targets)
+        ).sum() / num_fg
+        return loss_iou
+
+    
---
+++ losses/yolox_obj/__init__.py
@@ -0,0 +1,71 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from .. import yolox_utils as yolox_utils
+
+
+class Loss(nn.Module):
+    def __init__(self, loss_cfg):
+        super().__init__()
+        self.loss_cfg = loss_cfg
+        self.bcewithlog_loss = nn.BCEWithLogitsLoss(reduction="none")
+
+    def forward(self, y_hat, y):
+        return self.get_losses(**y_hat, y)
+    
+    def get_losses(self, imgs,
+            x_shifts,
+            y_shifts,
+            expanded_strides,
+            labels,
+            outputs,
+            origin_preds,
+            dtype, y):
+        bbox_preds = outputs[:, :, :4]  # [batch, n_anchors_all, 4]
+        obj_preds = outputs[:, :, 4].unsqueeze(-1)  # [batch, n_anchors_all, 1]
+        cls_preds = outputs[:, :, 5:]  # [batch, n_anchors_all, n_cls]
+
+        origin_preds = torch.cat(origin_preds, 1)
+        fg_masks = []
+        obj_targets = []
+        num_fg = 0.0
+        num_gts = 0.0
+    
+        # calculate targets
+        mixup = labels.shape[2] > 5
+        if mixup:
+            label_cut = labels[..., :5]
+        else:
+            label_cut = labels
+        nlabel = (label_cut.sum(dim=2) > 0).sum(dim=1)  # number of objects
+        total_num_anchors = outputs.shape[1]
+
+        for batch_idx in range(outputs.shape[0]):
+            num_gt = int(nlabel[batch_idx])
+            num_gts += num_gt
+            if num_gt == 0:
+                obj_target = outputs.new_zeros((total_num_anchors, 1))
+            else:
+                gt_bboxes_per_image = labels[batch_idx, :num_gt, 1:5]
+                gt_classes = labels[batch_idx, :num_gt, 0]
+                bboxes_preds_per_image = bbox_preds[batch_idx]
+                (gt_matched_classes, fg_mask, pred_ious_this_matching, matched_gt_inds, num_fg_img) = yolox_utils.get_assignments(batch_idx, num_gt, total_num_anchors, gt_bboxes_per_image, gt_classes,
+                bboxes_preds_per_image, expanded_strides, x_shifts, y_shifts,
+                cls_preds, bbox_preds, obj_preds, labels, imgs)
+                
+                obj_target = fg_mask.unsqueeze(-1)
+
+            obj_targets.append(obj_target)
+            fg_masks.append(fg_mask)
+
+        
+        fg_masks = torch.cat(fg_masks, 0)
+        obj_targets = torch.cat(obj_targets, 0)
+        num_fg = max(num_fg, 1)
+
+        loss_obj = (
+            self.bcewithlog_loss(obj_preds.view(-1, 1), obj_targets)
+        ).sum() / num_fg
+        return loss_obj
+
+    
